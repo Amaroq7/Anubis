d3ab1b11819897fee75296e4d200fcf70b8c33df
Karol Szuster <karolsz9898@gmail.com>
2021-03-26 02:25:39 +0100

    index on main: 0015fd7 some

---
+++ include/Utils.hpp
@@ -0,0 +1,8 @@
+//
+// Created by karol on 3/23/21.
+//
+
+#ifndef METAMOD_UTILS_HPP
+#define METAMOD_UTILS_HPP
+
+#endif // METAMOD_UTILS_HPP
---
+++ public/IHelpers.hpp
@@ -0,0 +1,8 @@
+//
+// Created by karol on 3/23/21.
+//
+
+#ifndef METAMOD_IHELPERS_HPP
+#define METAMOD_IHELPERS_HPP
+
+#endif // METAMOD_IHELPERS_HPP
