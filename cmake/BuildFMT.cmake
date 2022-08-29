d6d6e3cff626312483f7becbe51f7488c82d0375
Karol Szuster <karolsz9898@gmail.com>
2022-08-29 17:47:16 +0200

    Update fmt lib
    Signed-off-by: Karol Szuster <karolsz9898@gmail.com>

--- cmake/BuildFMT.cmake
+++ cmake/BuildFMT.cmake
@@ -5,7 +5,7 @@ include(FetchContent)
 FetchContent_Declare(
         fmtlib
         GIT_REPOSITORY https://github.com/fmtlib/fmt.git
-        GIT_TAG 8.1.1
+        GIT_TAG 9.1.0
         GIT_SHALLOW ON
 )
 
